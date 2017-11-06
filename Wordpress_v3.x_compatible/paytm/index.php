e36e5ad896106ac152723ea12186a9e26a05925b
aabhishekgoyal <abhishek.goyal@paytm.com>
2017-11-06 22:37:15 +0530

    Update index.php

--- Wordpress_v3.x_compatible/paytm/index.php
+++ Wordpress_v3.x_compatible/paytm/index.php
@@ -385,7 +385,7 @@ function woocommerce_paytm_init() {
 			$post_variables = Array(
             "MID" => $this -> merchantIdentifier,
             "ORDER_ID" => $order_id,
-            "CUST_ID" => $order -> billing_first_name,
+            "CUST_ID" => $email,
             "TXN_AMOUNT" => $amt,
             "CHANNEL_ID" => $this -> channel_id,
             "INDUSTRY_TYPE_ID" => $this -> industry_type,
@@ -475,7 +475,7 @@ function woocommerce_paytm_init() {
 			$paytm_args_array[] = "<input type='hidden' name='INDUSTRY_TYPE_ID' value='". $this -> industry_type ."'/>";
 			$paytm_args_array[] = "<input type='hidden' name='CHANNEL_ID' value='". $this -> channel_id ."'/>";
 			$paytm_args_array[] = "<input type='hidden' name='TXN_AMOUNT' value='". $amt ."'/>";
-			$paytm_args_array[] = "<input type='hidden' name='CUST_ID' value='". $order -> billing_first_name ."'/>";
+			$paytm_args_array[] = "<input type='hidden' name='CUST_ID' value='". $email ."'/>";
 			$paytm_args_array[] = "<input type='hidden' name='EMAIL' value='". $email ."'/>";
 			$paytm_args_array[] = "<input type='hidden' name='MOBILE_NO' value='". $mobile_no ."'/>";
 			
